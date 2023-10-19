fa047767dfe2bd84e584e27fc085f511a951ce87
Shoyeb Ansari <145090017+Shoyeb45@users.noreply.github.com>
2023-10-19 16:17:44 +0530

    Update FirstClass.java

--- Notes/FirstClass.java
+++ Notes/FirstClass.java
@@ -1,7 +1,7 @@
 class FirstClass  {
       public static void main(String args[])  {
-        System.out.print("The world\n") ;
+        System.out.print("Hello world\n") ;
      System.out.println("world") ;
-     System.out.print("The world is \nthe stand power of \ndio") ;
+     System.out.print("Hello world is first \ndprogramme") ;
       }    
 }
